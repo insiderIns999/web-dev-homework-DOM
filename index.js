86dd31b34acae7e231844204f6a4e456b9318e49
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-08-21 09:57:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { comments } from './modules/comments.js';
 import { initButtonLikes } from './modules/initButtonLikes.js';
 import { renderComments } from './modules/renderComments.js';
 
-"use strict";
+('use strict');
 export const userNameElement = document.querySelector('.add-form-name');
 export const userCommentElement = document.querySelector('.add-form-text');
 export const buttonSendElement = document.querySelector('.add-form-button');
@@ -10,17 +10,14 @@ export const ulElement = document.querySelector('.comments');
 
 export let likesCounter = 0;
 
-
-
 userNameElement.addEventListener('input', () => {
-  userNameElement.style.backgroundColor = '#fff';
+    userNameElement.style.backgroundColor = '#fff';
 });
 userCommentElement.addEventListener('input', () => {
-  userCommentElement.style.backgroundColor = '#fff';
+    userCommentElement.style.backgroundColor = '#fff';
 });
-
 
 renderComments();
 
-console.log("It works!");
+console.log('It works!');
 console.log('Complete');
