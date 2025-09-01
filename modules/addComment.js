5ac186f6408039d14fcde8408a1fa5aa10541bce
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-09-01 13:12:46 +0300

    Add files via upload

--- modules/addComment.js
+++ modules/addComment.js
@@ -1,47 +1,64 @@
-import { userNameElement, userCommentElement, likesCounter } from '../index.js';
-import { initButtonLikes } from './initButtonLikes.js';
-import { replaceAllTags } from './replaceAll.js';
-import { fetchGETAndRenderComments } from './fetchGETAndRenderComments.js';
+import { userNameElement, userCommentElement, likesCounter } from "../index.js";
+import { initButtonLikes } from "./initButtonLikes.js";
+import { replaceAllTags } from "./replaceAll.js";
+import { fetchGETAndRenderComments } from "./fetchGETAndRenderComments.js";
 
 export function addComment() {
-    if (
-        userNameElement.value == '' ||
-        userNameElement.value == ' ' ||
-        userCommentElement.value == '' ||
-        userCommentElement.value == ' '
-    ) {
-        userNameElement.style.backgroundColor = '#f00';
-        userCommentElement.style.backgroundColor = '#f00';
-        alert('Пожалуйста заполните все поля');
-    } else {
-        const blockLoader = document.getElementById('comment-loader');
-        const blockForm = document.getElementsByClassName('add-form');
+  if (
+    userNameElement.value == "" ||
+    userNameElement.value == " " ||
+    userCommentElement.value == "" ||
+    userCommentElement.value == " "
+  ) {
+    userNameElement.style.backgroundColor = "#f00";
+    userCommentElement.style.backgroundColor = "#f00";
+    alert("Пожалуйста заполните все поля");
+  } else {
+    const blockLoader = document.getElementById("comment-loader");
+    const blockForm = document.getElementsByClassName("add-form");
 
-        blockForm[0].style.display = 'none';
-        blockLoader.style.display = 'block';
+    blockForm[0].style.display = "none";
+    blockLoader.style.display = "block";
 
-        const nameReplaced = replaceAllTags(userNameElement.value);
-        const commentReplaced = replaceAllTags(userCommentElement.value);
+    const userNameBeforeSending = userNameElement.value;
+    const userCommentBeforeSending = userCommentElement.value;
 
-        fetch('https://wedev-api.sky.pro/api/v1/oleg-gagarin/comments', {
-            method: 'POST',
-            body: JSON.stringify({
-                name: nameReplaced,
-                text: commentReplaced
-            })
-        })
-        .then((response) => {
-            return response.json();
-        })
-        .then(() => {
-            return fetchGETAndRenderComments();
-        })
-        .then(() => {
-            blockForm[0].style.display = 'flex';
-            blockLoader.style.display = 'none';
-        });
+    const nameReplaced = replaceAllTags(userNameElement.value);
+    const commentReplaced = replaceAllTags(userCommentElement.value);
 
-        /*
+    fetch("https://wedev-api.sky.pro/api/v1/oleg-gagarin/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        name: nameReplaced,
+        text: commentReplaced,
+        forceError: true,
+      }),
+    })
+      .then((response) => {
+        if (response.status == 201) return response.json();
+        else {
+          blockForm[0].style.display = "flex";
+          userNameElement.value = userNameBeforeSending;
+          userCommentElement.value = userCommentBeforeSending;
+          if (response.status == 400)
+            throw new Error("Слишком короткое имя и/или текст комментария");
+          if (response.status == 500) {
+            addComment();
+          }
+        }
+      })
+      .then(() => {
+        return fetchGETAndRenderComments();
+      })
+      .catch((error) => {
+        alert(error.message);
+      })
+      .finally(() => {
+        blockForm[0].style.display = "flex";
+        blockLoader.style.display = "none";
+      });
+
+    /*
         comments.push({
             userName: nameReplaced,
             commentText: commentReplaced,
@@ -50,8 +67,8 @@ export function addComment() {
             isLiked: false,
         });
         */
-        userNameElement.value = '';
-        userCommentElement.value = '';
-        initButtonLikes();
-    }
+    userNameElement.value = "";
+    userCommentElement.value = "";
+    initButtonLikes();
+  }
 }
--- modules/fetchGETAndRenderComments.js
+++ modules/fetchGETAndRenderComments.js
@@ -1,16 +1,26 @@
-import { updateComments } from './comments.js';
-import { renderComments } from './renderComments.js';
-import { titleH3 } from '../index.js';
+import { updateComments } from "./comments.js";
+import { renderComments } from "./renderComments.js";
+import { titleH3 } from "../index.js";
+import { getUserCommentDate } from "./userCommentDate.js";
 
 export const fetchGETAndRenderComments = () => {
-    return fetch('https://wedev-api.sky.pro/api/v1/oleg-gagarin/comments/')
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            updateComments(data.comments);
-            renderComments();
-            titleH3.style.display = 'none';
-        });
-
-}
+  return fetch("https://wedev-api.sky.pro/api/v1/oleg-gagarin/comments/")
+    .then((response) => {
+      /*if (
+        response.status == 200)*/ return response.json();
+      /*else {
+        if (response.status == 500)
+          throw new Error("Нет соединения с сервером");
+        if (response.status == 400) throw new Error("Что-то пошло не так");
+      }*/
+    })
+    .then((data) => {
+      updateComments(data.comments);
+      getUserCommentDate(data.comments.date);
+      renderComments();
+      titleH3.style.display = "none";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
