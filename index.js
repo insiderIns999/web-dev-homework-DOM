3c78028cf57a3ccc670ca3fbbe8e43f925e97d0f
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-10-06 12:49:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 import { renderCommentsList } from './modules/fetchGETAndRenderComments.js';
 import { authorizationForm } from './modules/formAuthorization.js';
-//import { takeAndRender } from './modules/api.js';
-import { addingComment } from './modules/addComment.js';
-import { authorizationForm } from './modules/formAuthorization.js';
-//import { buttonSendElement } from './modules/addComment.js';
 
 renderCommentsList()
-.then(() => {
-   // authorizationForm();
-})
-
-export const upload = document.getElementById('uploading-data');
-
-export const userNameElement = document.querySelector('.add-form-name');
-export const userCommentElement = document.querySelector('.add-form-text');
-
-renderCommentsList()
-//takeAndRender()
 .then(() => {
     authorizationForm();
-})
-.then(() => {
-    addingComment();
 })
 
 export const upload = document.getElementById('uploading-data');
@@ -38,6 +20,3 @@ export const updateUserName = (newUserName) => {
 
 console.log('It works!');
 console.log('Complete');
-
-
-
